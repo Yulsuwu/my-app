c9e9e5f88add77fce029d51c6a28ef35b50374be
yulsuwu <julio.merey@comunidad.unam.mx>
2025-03-04 21:25:23 -0600

    Se intento

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import './App.css';
+import {Button, FormControlLabel, FormGroup} from '@material-ui/core';
 
 function App() {
   return (
@@ -7,8 +9,20 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> practica 5.
         </p>
+        <Button variant="contained">Contained</Button>
+        <Button variant="contained" disabled>
+          Disabled
+        </Button>
+        <Button variant="contained" href="#contained-buttons">
+          Link
+        </Button>
+        <FormGroup>
+          <FormControlLabel control={<Checkbox defaultChecked />} label="Label" />
+          <FormControlLabel required control={<Checkbox />} label="Required" />
+          <FormControlLabel disabled control={<Checkbox />} label="Disabled" />
+        </FormGroup>
         <a
           className="App-link"
           href="https://reactjs.org"
